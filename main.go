ff792310eaafa03a06625a38ffaf760fe1355085
mateimmo14 <110963035+mateimmo14@users.noreply.github.com>
2026-03-30 17:23:47 +0300

    bugfix 1

--- main.go
+++ main.go
@@ -25,6 +25,6 @@ func main() {
 		go client.WritePump(r.Context())
 		client.ReadPump(r.Context())
 	})
-	mux.Handle("GET /", http.FileServer(http.Dir("./html/")))
+	mux.Handle("/", http.FileServer(http.Dir("./html/")))
 	http.ListenAndServe(":8080", mux)
 }
